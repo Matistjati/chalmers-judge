d2170a904a90458f7cd3840e9f2dacfa4f724642
Matistjati <matistjati@outlook.com>
2026-05-28 20:47:49 +0000

    Use newer django version

--- omogenjudge/settings/base.py
+++ omogenjudge/settings/base.py
@@ -100,7 +100,6 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'Europe/Stockholm'
-USE_L10N = False
 USE_I18N = True
 USE_TZ = True
 DATE_FORMAT = "Y-m-d"
